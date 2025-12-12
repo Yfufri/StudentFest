4074b67c63731368dd8f81d22259e965a28afc3e
Yfufri <theolannier50@gmail.com>
2025-12-12 10:48:00 +0100

    igbzd

--- app/views/home.php
+++ app/views/home.php
@@ -10,7 +10,80 @@
 </section>
 
 <section class="notre-equipe">
-    <h3>Présentation de l'équipe</h3>
+    <h3>Organisateurs</h3>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Marc LAPIERRE</div>
+            <p>DIRECTION</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Jean LAURENT</div>
+            <p>DIRECTION</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Jade LAHAYE</div>
+            <p>COMMUNICATION</p>
+        </div>
+    </div>
+</section>
+<section class="notre-equipe">
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Bruno VIRE</div>
+            <p>CHEF</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Maria LARUE</div>
+            <p>CHEF</p>
+        </div>
+    </div>
+
+    <h3>Cuisiniers</h3>
+</section>
+<section class="notre-equipe">
+    <h3>Agents de Sécurité</h3>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Sacha MAYER</div>
+            <p>CHEF SECURITE</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Michel LAVANT</div>
+            <p>AGENT SECURITE</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Camille DANIEL</div>
+            <p>AGENT SECURITE</p>
+        </div>
+    </div>
+</section>
+<section class="notre-equipe">
 
     <div class="membre">
         <img class="membre-image" src="assets/images/default-membre.png">
@@ -43,4 +116,6 @@
             <p>Developpeur</p>
         </div>
     </div>
+
+    <h3>Developpeurs</h3>
 </section>
