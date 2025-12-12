3292d05993dfcbc5b1c22164dd868508d4abe339
Hugo <hugobard35@gmail.com>
2025-12-12 14:15:52 +0100

    Ajout de la page Valeur

---
+++ valeur.php
@@ -0,0 +1,45 @@
+<table-val>
+        <tr>
+            <td val1="box">
+                <h2-val>Tous Ensemble</h2-val>
+                <p-val>Créer un environnement où chaque étudiant, quelles que soient son origine, sa filière ou ses capacités, se sent bienvenu et peut participer pleinement à l'événement.</p-val>
+                <img src="assets/valeur1.jpg" alt="image valeur1">
+            </td>
+            <td val2="box">
+                <h2-val>Montre tes Talents</h2-val>
+                <p-val>Offrir une plateforme pour que tu puisse exprimer leurs talents artistiques, leurs idées innovantes et leurs passions, encourageant l'originalité et la diversité des formes d'art.</p-val>
+                <img src="assets/valeur2.png" alt="image valeur2">
+            </td>
+        </tr>
+        <tr>
+            <td val3="box">
+                <h2-val>Planète Friendly</h2-val>
+                <p-val>S'engager à minimiser l'impact environnemental du festival (gestion des déchets, des fournisseurs), faisant de la durabilité une priorité.</p-val>
+                <img src="assets/valeur3.png" alt="image valeur3">
+            </td>
+            <td val4="box">
+                <h2-val>Qualité de Vie</h2-val>
+                <p-val>Tout est facile et accessible. Les bénévoles sont là pour faciliter ton expérience dans le festival. Confort et simplicité garantis partout, pour tous.</p-val>
+                <img src="assets/valeur4.png" alt="image valeur4">
+            </td>
+        </tr>
+        <tr>
+            <td val5="box">
+                <h2-val>Innovation et Créativité</h2-val>
+                <p-val>Faciliter les rencontres et encourager les échanges authentiques entre tous les festivaliers.</p-val>
+                <img src="assets/valeur5.png" alt="image valeur5">
+            </td>
+            <td val6="box">
+                <h2-val>Camaraderie</h2-val>
+                <p-val>Cultiver un sentiment de communauté chaleureux, basé sur l'entraide et le respect mutuel.</p-val>
+                <img src="assets/valeur6.png" alt="image valeur6">
+            </td>
+        </tr>
+        <tr>
+            <td val7="box">
+                <h2-val>Un ambiance de ouf</h2-val>
+                <p-val>Créer une atmosphère unique et électrisante, garantissant une ambiance inoubliable.</p-val>
+                <img src="assets/valeur7.png" alt="image valeur7">
+            </td>
+        </tr>
+    </table-val>
