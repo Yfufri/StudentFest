96c0ee58b63b4635390fe517047bb4b253c1c125
Yfufri <theolannier50@gmail.com>
2025-12-12 10:32:03 +0100

    equipe debut css

---
+++ app/views/layout-head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>StudentFest</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
