d064a0506494a77c87fd6853ee22694ffa3c1229
Yfufri <theolannier50@gmail.com>
2025-12-12 10:13:22 +0100

    equipe sans css

--- app/views/home.php
+++ app/views/home.php
@@ -1,2 +1,35 @@
+<section class="notre-equipe">
+    <h3>Présentation de l'équipe</h3>
 
-hello world
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Hugo BARDIN</div>
+            <p>Developpeur</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Octave ROUFFAUD</div>
+            <p>Developpeur</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Kenan GUERIN</div>
+            <p>Fraise</p>
+        </div>
+    </div>
+
+    <div class="membre">
+        <img class="membre-image" src="assets/images/default-membre.png">
+        <div class="membre-text">
+            <div class="membre-name">Théo LANNIER</div>
+            <p>Developpeur</p>
+        </div>
+    </div>
+</section>
