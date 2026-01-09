65ef6d1955ba3e5a9a1e89ab813076178d4e543f
Hugo <hugobard35@gmail.com>
2026-01-09 09:49:36 +0100

    refonte valeur.php

--- app/views/valeur.php
+++ app/views/valeur.php
@@ -3,51 +3,56 @@
         <h3>Nos Valeurs</h3>
         <p>Découvrez les principes qui définissent StudentFest</p>
 
-        <table-val>
-            <tr>
-                <td class="val">
+        <div class="valGrid">
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Tous Ensemble</h4>
                     <p>Créer un environnement où chaque étudiant, quelles que soient son origine, sa filière ou ses capacités, se sent bienvenu et peut participer pleinement à l'événement.</p>
-                    <img src="assets/images/valeurs/valeur1.jpg" alt="image valeur1">
-                </td>
-                <td class="val">
+                </div>
+            </div>
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Montre tes Talents</h4>
                     <p>Offrir une plateforme pour que tu puisse exprimer leurs talents artistiques, leurs idées innovantes et leurs passions, encourageant l'originalité et la diversité des formes d'art.</p>
-                    <img src="assets/images/valeurs/valeur2.png" alt="image valeur2">
-                </td>
-            </tr>
-            <tr>
-                <td class="val">
+                </div>
+            </div>
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Planète Friendly</h4>
                     <p>S'engager à minimiser l'impact environnemental du festival (gestion des déchets, des fournisseurs), faisant de la durabilité une priorité.</p>
-                    <img src="assets/images/valeurs/valeur3.png" alt="image valeur3">
-                </td>
-                <td class="val">
+                </div>
+            </div>
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Qualité de Vie</h4>
                     <p>Tout est facile et accessible. Les bénévoles sont là pour faciliter ton expérience dans le festival. Confort et simplicité garantis partout, pour tous.</p>
-                    <img src="assets/images/valeurs/valeur4.png" alt="image valeur4">
-                </td>
-            </tr>
-            <tr>
-                <td class="val">
+                </div>
+            </div>
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Innovation et Créativité</h4>
                     <p>Faciliter les rencontres et encourager les échanges authentiques entre tous les festivaliers.</p>
-                    <img src="assets/images/valeurs/valeur5.png" alt="image valeur5">
-                </td>
-                <td class="val">
+                </div>
+            </div>
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Camaraderie</h4>
                     <p>Cultiver un sentiment de communauté chaleureux, basé sur l'entraide et le respect mutuel.</p>
-                    <img src="assets/images/valeurs/valeur6.png" alt="image valeur6">
-                </td>
-            </tr>
-            <tr>
-                <td class="val">
+                </div>
+            </div>
+            <div class="val">
+                <img src="assets/images/valeurs/valeur.webp" alt="image valeur1">
+                <div class="imageDescription">
                     <h4>Une ambiance de ouf</h4>
                     <p>Créer une atmosphère unique et électrisante, garantissant une ambiance inoubliable.</p>
-                    <img src="assets/images/valeurs/valeur7.png" alt="image valeur7">
-                </td>
-            </tr>
-        </table-val>
+                </div>
+            </div>
         <?php include 'footer.php'; ?>
     </form>
 </div>
