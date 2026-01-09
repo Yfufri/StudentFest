322c70571306546c67ed365ce1b4196c1a2b8f56
Hugo <hugobard35@gmail.com>
2026-01-09 08:44:08 +0100

    modifs valeurs

--- app/views/home.php
+++ app/views/home.php
@@ -158,34 +158,34 @@
     <h3>Developpeurs</h3>
 
     <div>
-        <h1-val>Nos Valeurs</h1-val>
+    <h3>Nos Valeurs</h3>
         <table-val>
-            <tr>
-                <td val1="box">
-                    <h2-val>Tous Ensemble</h2-val>
-                    <p-val>Créer un environnement où chaque étudiant, quelles que soient son origine, sa filière ou ses capacités, se sent bienvenu et peut participer pleinement à l'événement.</p-val>
-                    <img src="assets/valeur1.jpg" alt="image valeur1">
-                </td>
-                <td val2="box">
-                    <h2-val>Montre tes Talents</h2-val>
-                    <p-val>Offrir une plateforme pour que tu puisse exprimer leurs talents artistiques, leurs idées innovantes et leurs passions, encourageant l'originalité et la diversité des formes d'art.</p-val>
-                    <img src="assets/valeur2.png" alt="image valeur2">
-                </td>
-            </tr>
-            <tr>
-                <td val3="box">
-                    <h2-val>Planète Friendly</h2-val>
-                    <p-val>S'engager à minimiser l'impact environnemental du festival (gestion des déchets, des fournisseurs), faisant de la durabilité une priorité.</p-val>
-                    <img src="assets/valeur3.png" alt="image valeur3">
-                </td>
-                <td val4="box">
-                    <h2-val>Qualité de Vie</h2-val>
-                    <p-val>Tout est facile et accessible. Les bénévoles sont là pour faciliter ton expérience dans le festival. Confort et simplicité garantis partout, pour tous.</p-val>
-                    <img src="assets/valeur4.png" alt="image valeur4">
-                </td>
-            </tr>
-        </table-val>
-        <a href="?page=valeur">Voir plus</a>
+        <tr>
+            <td class="val">
+                <h4>Tous Ensemble</h4>
+                <p>Créer un environnement où chaque étudiant, quelles que soient son origine, sa filière ou ses capacités, se sent bienvenu et peut participer pleinement à l'événement.</p>
+                <img src="assets/images/valeurs/valeur1.jpg" alt="image valeur1">
+            </td>
+            <td class="val">
+                <h4>Montre tes Talents</h4>
+                <p>Offrir une plateforme pour que tu puisse exprimer leurs talents artistiques, leurs idées innovantes et leurs passions, encourageant l'originalité et la diversité des formes d'art.</p>
+                <img src="assets/images/valeurs/valeur2.png" alt="image valeur2">
+            </td>
+        </tr>
+        <tr>
+            <td class="val">
+                <h4>Planète Friendly</h4>
+                <p>S'engager à minimiser l'impact environnemental du festival (gestion des déchets, des fournisseurs), faisant de la durabilité une priorité.</p>
+                <img src="assets/images/valeurs/valeur3.png" alt="image valeur3">
+            </td>
+            <td class="val">
+                <h4>Qualité de Vie</h4>
+                <p>Tout est facile et accessible. Les bénévoles sont là pour faciliter ton expérience dans le festival. Confort et simplicité garantis partout, pour tous.</p>
+                <img src="assets/images/valeurs/valeur4.png" alt="image valeur4">
+            </td>
+        </tr>
+    </table-val>
+        <a href="?page=valeurs">Voir plus</a>
     </div>
 
 </section>
