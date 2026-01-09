94a727473088e5c690c1a139053b89cba8afe0d6
Kenan <kenan.guerin.etu@univ-lemans.fr>
2026-01-09 08:45:21 +0100

    fix homecontroller

--- app/controllers/homecontroller.php
+++ app/controllers/homecontroller.php
@@ -3,6 +3,6 @@ require "app/views/layout-head.php";
 require "app/views/header.php";
 require "app/views/home.php";
 require "app/controllers/contact.php";
-require "app/views/newsletter.php"
+require "app/views/newsletter.php";
 require "app/views/footer.php";
 ?>
