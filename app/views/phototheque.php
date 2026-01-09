558574879748b921177af4bfa64bdd166a7b2e9f
Hugo <hugobard35@gmail.com>
2026-01-09 09:29:17 +0100

    modif chemin photos phototheque

--- app/views/phototheque.php
+++ app/views/phototheque.php
@@ -6,27 +6,27 @@
             <tr>
                 <td class="photo1">
                     <h2>Festival 2018</h2>
-                    <img src="assets/photothèque1.jpg" alt="image photothèque1">
+                    <img src="assets/images/phototheque/photothèque1.jpg" alt="image photothèque1">
                 </td>
                 <td class="photo2">
                     <h2>Festival 2021</h2>
-                    <img src="assets/photothèque2.jpg" alt="image photothèque2">
+                    <img src="assets/images/phototheque/photothèque2.jpg" alt="image photothèque2">
                 </td>
             </tr>
             <tr>
                 <td class="photo3">
                     <h2>Festival 2022</h2>
-                    <img src="assets/photothèque3.jpg" alt="image photothèque3">
+                    <img src="assets/images/phototheque/photothèque3.jpg" alt="image photothèque3">
                 </td>
                 <td class="photo4">
                     <h2>Festival 2023</h2>
-                    <img src="assets/photothèque4.jpg" alt="image photothèque4">
+                    <img src="assets/images/phototheque/photothèque4.jpg" alt="image photothèque4">
                 </td>
             </tr>
             <tr>
                 <td class="photo5">
                     <h2>Festival 2024</h2>
-                    <img src="assets/photothèque5.jpg" alt="image photothèque5">
+                    <img src="assets/images/phototheque/photothèque5.jpg" alt="image photothèque5">
                 </td>
             </tr>
         </table>
